982c7fa9cbde83192f193b74976841a1e2212ab9
chiro45 <luciano.chiroli@gmail.com>
2025-06-05 19:51:27 -0300

    first commit

---
+++ src/main/java/com/example/auth0UsersAndRoles/Auth0UsersAndRolesApplication.java
@@ -0,0 +1,13 @@
+package com.example.auth0UsersAndRoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0UsersAndRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0UsersAndRolesApplication.class, args);
+	}
+
+}
